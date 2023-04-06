d4d10b7d8c13399746d8dbb642d40f9d64d7ead2
sw-sys <sheldonkorpet@gmail.com>
2023-04-06 19:33:58 +0100

    finish nav bar

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const burger = document.querySelector("nav svg")
 
 // when burger is clicked, run func
 burger.addEventListener("click", () => {
-    burger.classList.toggle("active")
-})
+    if (burger.classList.contains("active")) {
+        gsap.to(".links", { x: "100%" });
+    } else {
+        gsap.to(".links", { x: "0%" });
+    }
+    burger.classList.toggle("active");
+});
